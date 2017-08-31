a311259b6e5b1a1160f8b1eee1a7c49cb04d1049
Anthony Navarro <anavarroa@three29.com>
2017-08-31 10:33:41 -0700

    +Add task seeder

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -8,26 +8,8 @@
   crossorigin="anonymous">
 </script>
 
-<!-- Latest compiled and minified CSS -->
-<link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-    crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link
-    rel="stylesheet"
-    href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-    integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-    crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script
-    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-    crossorigin="anonymous">
-</script>
+<!-- Bootstrap -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 <head>
     <title>{{ config( 'app.name' ) }}</title>
@@ -51,6 +33,7 @@
         @if( ! Auth::check() )
             <a href="{{ route( 'auth.login' ) }}" class="login-button align-right btn">Login</a>
         @else
+            <p class="align-right">Welcome, {{ Auth::user()->getName() }}</p>
             <a href="{{ route( 'logout' ) }}" onclick="event.preventDefault(); document.getElementById( 'logout-form' ).submit();" class="logout-button align-right btn">Logout</a>
             <form id="logout-form" action="{{ route( 'logout' ) }}" method="POST" style="display: none;">
                 {{ csrf_field() }}
