2e6f22d7d3bc4b403af49abd738a543e054e9fde
Anthony Navarro <anavarroa@three29.com>
2017-09-05 15:49:46 -0700

    Fix insertion to accept full dev name rather than first name only

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,24 +5,24 @@
 
 
     <h4>Task Information:</h4>
-    <form action={{ route( 'tasks.store' ) }} method="POST">
+    <form action="{{ route( 'tasks.store' ) }}" method="POST">
         {{ csrf_field() }}
 
         <h3 class="title-standout">Create Task</h3>
-        <label for="Task Name">Task Name</label><br>
+        <label for="task_name">Task Name</label><br>
         <input type="text" id="task_name" name="task_name" class="form-control task-input" placeholder="Enter Task Name" required><br><br>
-        <label for="Client Name">Client Name</label><br>
+        <label for="client">Client Name</label><br>
         <input type="text" id="client" name="client" class="form-control task-input" placeholder="Enter Client Name" required><br><br>
-        <label for="Developer Name">Developers</label><br>
+        <label for="developers">Developers</label><br>
         <select id="developers" name="developers" class="form-control task-input">
             <option value="default">Select One</option>
             @foreach( $developers_list as $developer )
-                <option value={{ $developer }}>{{ $developer }}</option>
+                <option value="{{ $developer }}">{{ $developer }}</option>
             @endforeach
         </select><br><br>
-        <label for="Hours Worked">Hours Worked</label><br>
+        <label for="hours_worked">Hours Worked</label><br>
         <input type="text" id="hours_worked" name="hours_worked" class="form-control task-input" placeholder="Enter Hours Worked" required><br><br>
-        <label for="Hours To Build">Hours To Build</label><br>
+        <label for="hours_to_build">Hours To Build</label><br>
         <input type="text" id="hours_to_build" name="hours_to_build" class="form-control task-input" placeholder="Enter Hours To Build" required><br><br>
 
         <button class="btn add-task-button">Create New Task</button>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -20,25 +20,25 @@
         </div>
 
         <div class="col-md-6">
-            <form action={{ route( 'tasks.update', $task ) }} method="POST">
+            <form action="{{ route( 'tasks.update', $task ) }}" method="POST">
                 {{ csrf_field() }}
                 {{ method_field( 'PUT' ) }}
 
                 <h3 class="title-standout">Edit Task</h3>
-                <label for="Task Name">Updated Task Name</label><br>
+                <label for="task_name">Updated Task Name</label><br>
                 <input type="text" id="task_name" name="task_name" class="form-control task-input" placeholder="" required><br><br>
-                <label for="Client Name">Updated Client Name</label><br>
+                <label for="client">Updated Client Name</label><br>
                 <input type="text" id="client" name="client" class="form-control task-input" placeholder="" required><br><br>
-                <label for="Developer Name">Updated Developers</label><br>
+                <label for="developers">Updated Developers</label><br>
                 <select id="developers" name="developers" class="form-control task-input">
                     <option value="default">Select One</option>
                     @foreach( $developers_list as $developer )
-                        <option value={{ $developer }}>{{ $developer }}</option>
+                        <option value="{{ $developer }}">{{ $developer }}</option>
                     @endforeach
                 </select><br><br>
-                <label for="Hours Worked">Updated Hours Worked</label><br>
+                <label for="hours_worked">Updated Hours Worked</label><br>
                 <input type="text" id="hours_worked" name="hours_worked" class="form-control task-input" placeholder="" required><br><br>
-                <label for="Hours To Build">Updated Hours To Build</label><br>
+                <label for="hours_to_build">Updated Hours To Build</label><br>
                 <input type="text" id="hours_to_build" name="hours_to_build" class="form-control task-input" placeholder="" required><br><br>
 
                 <button class="btn add-task-button">Update Task Information</button>
