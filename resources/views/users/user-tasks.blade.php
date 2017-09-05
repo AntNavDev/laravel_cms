d2f76ac5128c3e53465330d2fc790a921916f30d
Anthony Navarro <anavarroa@three29.com>
2017-09-05 09:42:21 -0700

    Update task UI

--- resources/views/users/user-tasks.blade.php
+++ resources/views/users/user-tasks.blade.php
@@ -5,6 +5,9 @@
         <tr>
             <th>Task</th>
             <th>Client</th>
+            <th>Developers</th>
+            <th>Hours Worked</th>
+            <th>Hours To Build</th>
             <th class="action-header">Action</th>
         </tr>
         @foreach( $tasks as $task )
@@ -12,6 +15,9 @@
                 <tr>
                     <td>{{ $task->task_name }}</td>
                     <td>{{ $task->client }}</td>
+                    <td>{{ $task->developers }}</td>
+                    <td>{{ $task->hours_worked }}</td>
+                    <td>{{ $task->hours_to_build }}</td>
                     <td class="align-right">
                         <a href="tasks/{{ $task->id }}/edit" class="btn task-button edit-task-button">Edit</a>
                         <form action="tasks/{{ $task->id }}" method="POST" style="display: inline-block;">
