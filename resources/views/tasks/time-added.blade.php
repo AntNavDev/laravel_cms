a7fe93081f5e8cb47402c713bcb7ad7ea812d91d
Anthony Navarro <anavarroa@three29.com>
2017-09-07 15:43:50 -0700

    change where add time is rendered

--- resources/views/tasks/time-added.blade.php
+++ resources/views/tasks/time-added.blade.php
@@ -26,3 +26,29 @@
     </div>
 
 </div>
+
+<div class="row">
+    <div class="col-md-12">
+        <h3>Enter Task Details</h3>
+        <form action="{{ route( 'task_time.store', $task ) }}" method="POST">
+            {{ csrf_field() }}
+
+            <input type="hidden" id="task_id" name="task_id" value="{{ $task->id }}">
+
+            <label for="developer">Developer</label>
+            <select id="developer" name="developer" class="form-control task-input">
+                <option value="default">Select One</option>
+                @foreach( $developers_list as $developer )
+                    <option value="{{ $developer }}">{{ $developer }}</option>
+                @endforeach
+            </select>
+            <label for="description">Description of Work</label>
+            <input type="text" id="description" name="description" class="form-control task-input" placeholder="" required>
+            <label for="hours">Hours to Add</label>
+            <input type="text" id="hours" name="hours" class="form-control task-input" placeholder="" required>
+
+            <br><br>
+            <button class="btn add-task-button">Update Time Logged</button>
+        </form>
+    </div>
+</div>
